1e036a051f52a8186844e657061b9ab7842dbc4c
NahomMA <nbirhanm@gmail.com>
2026-08-11 22:38:41 -0400

    just node

--- 100-days-dsa-ml-journey/ml_practice/linear_regression.py
+++ 100-days-dsa-ml-journey/ml_practice/linear_regression.py
@@ -1,12 +1,12 @@
 
-# %% 
+# %%
 """
 Linear Regression
 Convention: https://www.deeplearningbook.org/contents/ml.html (Goodfellow et. al.)
 
 Given, x, y, w, b of dim (n), 1, (n,1), (1)  -------- y = w^T * x + b  ----------------- 
 
-For m test examples, 
+For m test examples,
 
 y(test) = X(test)@w + b, metrics: mse(test) = 1/m * (sum_i [ (y_pred(test) - y_true(test) ) ]_i  )
 
@@ -15,9 +15,9 @@ mse(test)  = 1/m * || y_pred(test) - y_true(test) || ** 2
 
 
 
-Grads: Minimize mse over train to find optimal w, 
+Grads: Minimize mse over train to find optimal w,
 
-Grad_w (mse_train) = 0 
+Grad_w (mse_train) = 0
 => Grad_w (1/m . || y_pred(train) - y_true(train) || ** 2) = 0
 => Grad_w (1/m . || X(train) * w - y_true(train) || ** 2 ) = 0
 => Grad_w (1/m . ( X(train) * w - y_true(train) ) ^T  ( X(train) * w - y_true(train) ) ) = 0
