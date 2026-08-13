293a294a2e8bcd78bd1e103450d5e1af29833334
NahomMA <nbirhanm@gmail.com>
2026-08-12 21:40:01 -0400

    adding node to a linked list

--- 100-days-dsa-ml-journey/dsa_lessons/linked_list_n.py
+++ 100-days-dsa-ml-journey/dsa_lessons/linked_list_n.py
@@ -4,9 +4,8 @@ class Node:
         self.next = None
 
 
-
-# take the head of the linked list and return the sum of the values
- ## ** iterative
+#take the head of the linked list and return the sum of the values
+## ** iterative
 def get_sum(head):
     ans = 0
     while head:
@@ -22,16 +21,40 @@ head = n1
 
 n1.next = n2
 n2.next = n3
+
 print(f'sum of values in Linked List {get_sum(head)}')
 
 
-# *** Recurssive
+#***Recurssive
 def get_sum(head):
     if not head:
         return 0
     return head.val + get_sum(head.next)
 
 print(f'll_values recursive sum {get_sum(head)}')
+
+
+
+#Date: 08/12/2026 start @ 9:08
+
+def add_node(prev_node, node_2add):
+    #assuming to add node at any point 
+    #1. first rightside
+    node_2add.next = prev_node.next 
+    #2.left side
+    prev_node.next = node_2add
+    return prev_node
+
+
+node_2add = Node(11)
+
+new_node = add_node(head,node_2add)
+
+node_index = 1
+while new_node:
+    print(f'node val at index {node_index}= {new_node.val}')
+    new_node = new_node.next
+    node_index = node_index +1
 
 
 
