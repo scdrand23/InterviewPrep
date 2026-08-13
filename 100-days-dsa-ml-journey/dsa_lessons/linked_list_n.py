6b8cf75a87d033eeaa87dbcaeb33876e757df073
NahomMA <nbirhanm@gmail.com>
2026-08-12 22:13:21 -0400

    delete the next node

--- 100-days-dsa-ml-journey/dsa_lessons/linked_list_n.py
+++ 100-days-dsa-ml-journey/dsa_lessons/linked_list_n.py
@@ -56,6 +56,40 @@ while new_node:
     new_node = new_node.next
     node_index = node_index +1
 
+def print_ll_vals(head):
+    node_index = 1
+    while head:
+        print(f'node val at index {node_index}= {head.val}')
+        head = head.next
+        node_index = node_index +1
+
+
+'''
+Deleting the next node is easy
+Technically deleting the next node given the current node as input
+
+'''
+
+d_node1 = Node(10)
+d_node2 = Node(20)
+d_node3 = Node(30)
+d_node4  = Node(40)
+
+d_nodes = add_node(add_node(add_node(d_node1,d_node2),d_node3),d_node4)
+
+
+def del_node(prev_node):
+    " technically delete the next node"
+    prev_node.next = prev_node.next.next
+    return prev_node
+
+print_ll_vals(d_nodes)
+
+#let us delete node with val = 30
+print(f'smoke test for deletion')
+
+
+print_ll_vals(del_node(d_nodes))
 
 
 
